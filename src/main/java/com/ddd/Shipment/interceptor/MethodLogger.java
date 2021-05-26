05e045c83a3092448005271ebd0fb6fcd224cfce
Justas <justaslescinskas@gmail.com>
2021-05-26 12:04:37 +0300

    feat(): Adding Interceptor on HTTP requests

---
+++ src/main/java/com/ddd/Shipment/interceptor/MethodLogger.java
@@ -0,0 +1,34 @@
+package com.ddd.Shipment.interceptor;
+
+import javax.interceptor.AroundInvoke;
+import javax.interceptor.Interceptor;
+import javax.interceptor.InvocationContext;
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerResponseContext;
+import javax.ws.rs.container.ContainerResponseFilter;
+import javax.ws.rs.ext.Provider;
+import java.io.IOException;
+import java.util.logging.FileHandler;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
+
+@Provider
+public class MethodLogger implements ContainerResponseFilter {
+    @Override
+    public void filter(ContainerRequestContext containerRequestContext, ContainerResponseContext containerResponseContext) throws IOException {
+        Logger logger = Logger.getLogger("UserActivityLog");
+        FileHandler fh;
+        try {
+            fh = new FileHandler("log.log");
+            logger.addHandler(fh);
+            SimpleFormatter formatter = new SimpleFormatter();
+            fh.setFormatter(formatter);
+            logger.info("User did request: " + containerRequestContext.getRequest());
+            logger.info("User did method: " + containerRequestContext.getMethod());
+
+        } catch (SecurityException | IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
